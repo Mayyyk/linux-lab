2ce73bc03c319ac7604267122483b77529fe78d8
Michał Midor <124244749+Mayyyk@users.noreply.github.com>
2025-04-17 10:16:53 +0200

    Created backup script

--- toolkit/backup.sh
+++ toolkit/backup.sh
@@ -1,1 +1,15 @@
-#
+#!/bin/bash
+
+if [ ! -d $HOME/backup ]; then
+	mkdir -p $HOME/backup
+	echo "Created directory ~/backup"
+fi
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+archive_name="backup-$timestamp.tar.gz"
+
+tar -czf $HOME/backup/$archive_name $HOME/Documents
+
+echo "[$timestamp] Created $archive_name from directory ~/Documents" >> $HOME/backup/backup.log
+
+
